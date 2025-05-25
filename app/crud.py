28f72fb4254fe1e2e6e287e021a9d36429a5a765
Enrique Quevedo <quevedo.enrique01@gmail.com>
2025-05-25 21:41:25 +0200

    Add Pydantic schemas and basic CRUD functions

---
+++ app/crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+def create_customer(db: Session, customer: schemas.CustomerCreate):
+    db_customer = models.Customer(**customer.dict())
+    db.add(db_customer)
+    db.commit()
+    db.refresh(db_customer)
+    return db_customer
+
+def create_product(db: Session, product: schemas.ProductCreate):
+    db_product = models.Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def create_invoice(db: Session, invoice: schemas.InvoiceCreate):
+    db_customer = db.query(models.Customer).filter(models.Customer.id == invoice.customer_id).first()
+    db_products = db.query(models.Product).filter(models.Product.id.in_(invoice.product_ids)).all()
+
+    db_invoice = models.Invoice(customer=db_customer, products=db_products)
+    db.add(db_invoice)
+    db.commit()
+    db.refresh(db_invoice)
+    return db_invoice
